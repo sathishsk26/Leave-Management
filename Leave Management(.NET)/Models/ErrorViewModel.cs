b6466ad468c79ffbaa28343613acc64fc18dc9c2
SATHISH_SK <kumarsathish92777@outlook.com>
2022-09-27 11:00:22 +0530

    Add project files.

---
+++ Leave Management(.NET)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Leave_Management_.NET_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
